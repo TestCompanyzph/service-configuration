1592aa207d05f8ae8d2417e9132b64ad80295daa
zhangpenghui <zhangpenghui>
2017-11-15 18:58:30 +0800

    新建项目

---
+++ src/main/java/com/zph/usedCar/configuration/ServiceConfigurationApplication.java
@@ -0,0 +1,12 @@
+package com.zph.usedCar.configuration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceConfigurationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceConfigurationApplication.class, args);
+	}
+}
